c4628ed9eed6ef04f0e599c1c41ebe777fcca992
Matt Novoselov <59065228+matt-novoselov@users.noreply.github.com>
2023-12-08 13:47:49 +0100

    Initial upload

---
+++ MarsMonkey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarsMonkey
+//
+//  Created by Matt Novoselov on 08/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Mariia!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarsMonkey/MarsMonkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsMonkeyApp.swift
+//  MarsMonkey
+//
+//  Created by Matt Novoselov on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MarsMonkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
