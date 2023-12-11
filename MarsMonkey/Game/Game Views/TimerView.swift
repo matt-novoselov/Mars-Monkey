1ecf3c84b3fe4b7769a7fc9f6c17969df9199862
Gabriele Perillo <gabry.perillo@gmail.com>
2023-12-11 12:07:28 +0100

    Timer Model and TimerView

---
+++ MarsMonkey/Game/Game Views/TimerView.swift
@@ -0,0 +1,29 @@
+//
+//  TimerView.swift
+//  MarsMonkey
+//
+//  Created by Gabriele Perillo on 11/12/23.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    @ObservedObject var timerModel = TimerModel()
+    
+    var body: some View {
+        VStack {
+            StrokeText(text: "\(timerModel.formattedTime)", strokeWidth: 1)
+                .font(Font.custom("RedBurger", size: 48))
+                .foregroundColor(.mmPink)
+        }
+        
+        
+    }
+}
+
+#Preview {
+    TimerView()
+}
+
+
+
---
+++ MarsMonkey/Models/Timer Model.swift
@@ -0,0 +1,60 @@
+//
+//  Timer Model.swift
+//  MarsMonkey
+//
+//  Created by Gabriele Perillo on 11/12/23.
+//
+
+import SwiftUI
+import Foundation
+
+class TimerModel: ObservableObject {
+    @Published var seconds: Int = 120
+    
+    var formattedTime: String {
+            let minutes = seconds / 60
+            let seconds = self.seconds % 60
+            return String(format: "%01d:%02d", minutes, seconds)
+        }
+    
+    private var timer: Timer?
+
+    // To inizialize the timer
+    init() {
+        startTimer()
+    }
+    
+    // This function starts, decrement and increment the timer in some cases
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if self.seconds > 0 {
+                self.seconds -= 1
+            } else {
+                self.timer?.invalidate()
+            }
+
+            // Esempio: Decrementa di 10 secondi in certe circostanze
+            if self.seconds == 60 {
+                self.decrementTimer(by: 10)
+            }
+
+            // Esempio: Decrementa di 10 secondi in un altro caso
+            if self.seconds == 30 {
+                self.decrementTimer(by: 10)
+            }
+        }
+    }
+
+    func stopTimer() {
+        timer?.invalidate()
+    }
+
+    func decrementTimer(by amount: Int) {
+        if seconds > amount {
+            seconds -= amount
+        } else {
+            seconds = 0
+            stopTimer()
+        }
+    }
+}
