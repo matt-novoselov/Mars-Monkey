99da16657103985a18f2a9857ec1679db11ba631
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2023-12-11 12:18:54 +0100

    Added asteroid generation and collision

--- MarsMonkey/Game/GameScene.swift
+++ MarsMonkey/Game/GameScene.swift
@@ -31,9 +31,13 @@ class GameScene: SKScene{
     var joystickPosY: CGFloat = 0 // var representing how far the joystick was dragged on Y axis
     
     override func didMove(to view: SKView) {
+        
+        physicsWorld.contactDelegate = self
+        
         scene?.size = CGSize(width: 1179, height: 2556) // Set scene's resolution
         
         self.setUpGame() // Set up game in GameLogic's function
+        self.setUpPhysicsWorld() // Set up PhysicsWorld
         
         backgroundColor = .mmUIBackground
         
@@ -45,13 +49,20 @@ class GameScene: SKScene{
         player.position = CGPoint(x: size.width/2, y: size.height/2)
         player.zPosition = 10
         player.setScale(0.05)
+        player.physicsBody = SKPhysicsBody(circleOfRadius: 25)
+        player.physicsBody?.affectedByGravity = false
+        
+        // Setting CollisionBitMask, CategoryBitMask and ContactBitMask for a Player
+        player.physicsBody?.collisionBitMask = InstanceCategory.player
+        player.physicsBody?.categoryBitMask = InstanceCategory.asteroid
+        player.physicsBody?.contactTestBitMask = InstanceCategory.asteroid
         
         // Limit player's movement on X axis
         let xRange = SKRange(lowerLimit: 0 + 25, upperLimit: frame.width - 25)
         let xConstraint = SKConstraint.positionX(xRange)
         self.player.constraints = [xConstraint]
         
-        addChild(player)
+        addChild(self.player)
     }
     
     // Joystick becomes active after user touches the screen
@@ -95,11 +106,102 @@ class GameScene: SKScene{
         }
     }
     
+    
 }
 
 // Game Scene Set Up
 extension GameScene{
     private func setUpGame() {
         self.gameLogic.setUpGame()
+        self.startAsteroidsCycle()
+    }
+    private func setUpPhysicsWorld() {
+        physicsWorld.gravity = CGVector(dx: 0, dy: -0.9)
+        physicsWorld.contactDelegate = self
+    }
+}
+
+// Asteroids Creation
+extension GameScene {
+    
+    private func createAsteroid() {
+        let asteroidPosition = self.randomAsteroidPosition()
+        newAsteroid(at: asteroidPosition)
+    }
+    
+    private func randomAsteroidPosition() -> CGPoint {
+        let initialX: CGFloat = 25
+        let finalX: CGFloat = self.frame.width - 25
+        
+        let positionX = CGFloat.random(in: initialX...finalX)
+        let positionY = frame.height - 25
+        
+        return CGPoint(x: positionX, y: positionY)
+    }
+    
+    private func newAsteroid(at position: CGPoint) {
+        let newAsteroid = SKSpriteNode(imageNamed: "asteroid")
+        newAsteroid.name = "asteroid"
+        newAsteroid.setScale(0.25)
+        newAsteroid.zPosition = player.zPosition
+        newAsteroid.position = position
+        
+        newAsteroid.physicsBody = SKPhysicsBody(circleOfRadius: 25.0)
+        newAsteroid.physicsBody?.affectedByGravity = true
+        
+        newAsteroid.physicsBody?.categoryBitMask = InstanceCategory.asteroid
+        newAsteroid.physicsBody?.contactTestBitMask = InstanceCategory.player
+        newAsteroid.physicsBody?.collisionBitMask = InstanceCategory.player
+        
+        addChild(newAsteroid)
+        
+        newAsteroid.run(SKAction.sequence([
+            SKAction.wait(forDuration: 5.0),
+            SKAction.removeFromParent()
+        ]))
+    }
+    
+}
+
+// Asteroids Cycle
+extension GameScene{
+    func startAsteroidsCycle() {
+        let createAsteroidAction = SKAction.run(createAsteroid)
+        let waitAction = SKAction.wait(forDuration: 5.0)
+        
+        let createAndWaitAction = SKAction.sequence([createAsteroidAction, waitAction])
+        let asteroidCycleAction = SKAction.repeatForever(createAndWaitAction)
+        
+        run(asteroidCycleAction)
+    }
+}
+
+//extension GameScene: SKPhysicsContactDelegate{
+//    func didBegin(_ contact: SKPhysicsContact) {
+//        let firstBody: SKPhysicsBody = contact.bodyA
+//        let secondBody: SKPhysicsBody = contact.bodyB
+//        
+//        if let node = firstBody.node, node.name == "asteroid"{
+//            node.removeFromParent()
+//            
+//        }
+//        if let node = secondBody.node, node.name == "asteroid"{
+//            node.removeFromParent()
+//        
+//        }
+//        
+//        print("Contact happened!")
+//    }
+//}
+
+extension GameScene: SKPhysicsContactDelegate {
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        let asteroidNode = contact.bodyA.node?.name == "asteroid" ? contact.bodyA.node : contact.bodyB.node
+
+        if let asteroid = asteroidNode {
+            asteroid.removeFromParent()
+            print("Asteroid removed due to contact with the player.")
+        }
     }
 }
